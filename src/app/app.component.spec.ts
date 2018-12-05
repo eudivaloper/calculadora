5dc339d549c9d1dc2e73ebbb418f77ea78f6b264
Jessica Lopes <jeesousa91@gmail.com>
2018-12-05 16:44:48 -0200

    implementando o serviço

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { CalculadoraModule } from './calculadora/calculadora.module'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        CalculadoraModule
+      ]
     }).compileComponents();
   }));
 
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { CalculadoraService } from './calculadora.service';
 
 describe('CalculadoraService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService]
+    });
+  });
 
   it('should be created', () => {
     const service: CalculadoraService = TestBed.get(CalculadoraService);
